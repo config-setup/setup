109d788fe6b3c3b9d45f791cba81d7a601d56337
Reggles <reginaldbeakes@gmail.com>
2024-12-02 22:08:12 -0500

    install.sh update

--- install.sh
+++ install.sh
@@ -7,70 +7,60 @@ function clone_or_pull {
     popd
   else
     git clone "$2" $1
-    if [ ! -z "$3" ]; then
-      eval $3
-    fi
   fi
 }
 
-apt update
-apt upgrade
+apt update -y
+apt upgrade -y
+apt install git software-properties-common python3-pip zsh tmux ninja-build gettext cmake unzip curl build-essential -y
+apt autoremove -y
 
-apt install software-properties-common
+### DO NOT TOUCH ###
+# Downloading zshrc
+rm $HOME/.zshrc
+curl https://raw.githubusercontent.com/Reggles44/conf/refs/heads/main/zshrc -o $HOME/.zshrc
+### DO NOT TOUCH ###
 
 # git
-apt install git
 git config --global user.name "Reggles"
 git config --global user.email "reginaldbeakes@gmail.com"
 git config --global credential.helper store
 git config --global init.defaultBranch main
 git config --global push.autoSetupRemote true
 
-# python
-apt install python3-pip
-if [ ! command -v pyenv ] 2>&1 >/dev/null; then
-  curl https://pyenv.run | bash
-fi
+clone_or_pull $HOME/.local/bin/ https://github.com/Reggles44/local-bin.git # .local/bin
+clone_or_pull $HOME/.fzf "\-\-depth 1 https://github.com/junegunn/fzf.git" # fzf
+clone_or_pull $HOME/.config/tmux https://github.com/Reggles44/tmux.git     # tmux config
+clone_or_pull $HOME/.tmux/plugins/tpm https://github.com/tmux-plugins/tpm  # tpm
+clone_or_pull $HOME/.nvim https://github.com/neovim/neovim                 # nvim
 
 # zsh and ohmyzsh
-apt install zsh
-if [ ! command -v omz ] 2>&1 >/dev/null; then
+if ! command -v omz 2>&1 >/dev/null; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
   omz update
 fi
 
-### DO NOT TOUCH ###
-# Downloading zshrc
-curl https://raw.githubusercontent.com/Reggles44/conf/refs/heads/main/zshrc -o $HOME/.zshrc
-### DO NOT TOUCH ###
+# pyenv
+if ! command -v pyenv 2>&1 >/dev/null; then
+  curl https://pyenv.run | bash
+fi
+
+# fzf
+~/.fzf/install --all
 
 # starship
-if [ ! command -v starship ] 2>&1 >/dev/null; then
+if ! command -v starship 2>&1 >/dev/null; then
   curl -sS https://starship.rs/install.sh | sh
 else
   starship preset nerd-font-symbols -o $HOME/.config/starship.toml
 fi
 
-# .local/bin
-clone_or_pull $HOME/.local/bin/ https://github.com/Reggles44/local-bin.git
-
-# fzf
-clone_or_pull $HOME/.fzf "\-\-depth 1 https://github.com/junegunn/fzf.git" ~/.fzf/install
-
-# tmux
-apt install tmux
-clone_or_pull $HOME/.config/tmux https://github.com/Reggles44/tmux.git
-clone_or_pull $HOME/.tmux/plugins/tpm https://github.com/tmux-plugins/tpm
-
 # nvim
-apt install ninja-build gettext cmake unzip curl build-essential
-clone_or_pull $HOME/.nvim https://github.com/neovim/neovim
-pushd $HOME/.
+pushd $HOME/.nvim
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 make install
 popd
-rm -rf $NEOVIM_DIR
 
 # node version manager
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
@@ -84,7 +74,7 @@ tar -xf "/tmp/go.tar.gz" -C $HOME/go/
 rm -f /tmp/go.gar.gz
 
 # rust install
-if [ ! command -v rustup 2>&1 >/dev/null]; then
+if ! command -v rustup 2>&1 >/dev/null; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 else
   rustup update
